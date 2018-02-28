7a00afe6c6d61d2528d8d7bb222095c81242d3b0
Guzzii <guzzii316@gmail.com>
2018-02-28 14:36:46 -0600

    add plot method to the client class

--- machina/engine.py
+++ machina/engine.py
@@ -1,11 +1,26 @@
 from machina.src.core import compute_cost
-from seaborn import set
 
+import seaborn as sns
 import matplotlib.pylab as plt
 
 
 class Client(object):
     def __init__(self, plans, n_member=1, self_contri=0, tax_fac=0.):
+        """
+        Initiate client object
+
+        Parameters
+        ----------
+        plans : :obj:`pandas.DataFrame`
+            DataFrame holds information for medical plans
+        n_member : int
+            the number of memebers in the family
+        self_contri : float
+            the amount funds planed to contribute to HSA account
+        tax_fac : float
+            tax bracket that the HSA contribution is coming from
+        """
+        self._fit = False
         self._n_mem = n_member
         self._tax = 1 - tax_fac
 
@@ -33,6 +48,15 @@ class Client(object):
             self._plans[self._plans.members == self._n_mem].copy()
 
     def fit(self, iter=300):
+        """
+        Compute the cost for each plan
+
+        Parameters
+        ----------
+        iter : int
+            the number of iterations to compute, the raw cost = 100 * iter
+        """
+        self._iter = iter
         self._match_plans()
 
         cols = ['name', 'members', 'cost_per_year', 'deductible',
@@ -40,19 +64,26 @@ class Client(object):
                 'self_contribution', 'tax_rate']
         self._sub_plans = self._sub_plans[cols]
 
-        set()
-        x = list(range(100, iter*100+1, 100))
-
         cost_dict = {}
         for row in self._sub_plans.itertuples():
             tup = map(float, row[3:])
             cost_dict[row[1]] = compute_cost(*tup, iter)
-            plt.plot(x, cost_dict[row[1]], label=row[1])
+
+        self._fit = True
+        self._cost_dict = cost_dict
+
+    def plot(self):
+        if not self.fit:
+            raise RuntimeError('Please run model fit first.')
+
+        sns.set()
+        x = list(range(100, self._iter*100 + 1, 100))
+
+        for key in self._cost_dict.keys():
+            plt.plot(x, self._cost_dict[key], label=key)
 
         plt.ylabel('Total cost (after-plan, after-tax)')
         plt.xlabel('Raw Medical Cost')
         plt.legend()
         plt.show()
         plt.close()
-
-        self._cost_dict = cost_dict
--- scripts/quickstart.py
+++ scripts/quickstart.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     plans = pd.read_csv(os.path.join(DIR, '../data/medical-plan-2017.csv'))
     client = ma.Client(plans, n_member=1, self_contri=1000, tax_fac=0.25)
     client.fit(300)
+    client.plot()
