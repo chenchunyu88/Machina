f6283ab664383c43887e32a134589e73ab20f691
Guzzii <zach.wz5247@me.com>
2018-03-25 19:35:53 -0500

    add back -stdlib flag

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, Extension, find_packages
 from sysconfig import get_paths
 
 
-boost_python_lib = '/usr/local/boost_1_65_1/stage/lib/'
+# boost_python_lib = '/usr/local/boost_1_65_1/stage/lib/'
 python_include = get_paths()['include']
 
-compile_args = ['-g', '-std=c++1z']  # '-stdlib=libc++', ]
+compile_args = ['-g', '-std=c++1z', '-stdlib=libc++', ]
 link_args = []  # ['-mmacosx-version-min=10.12']
 
 extension = Extension(name='machina.src.core',
                       sources=['machina/src/core.cpp'],
                       include_dirs=[python_include],
-                      library_dirs=[boost_python_lib],
+                      # library_dirs=[boost_python_lib],
                       libraries=['boost_python3'],
                       extra_compile_args=compile_args,
                       extra_link_args=link_args,
