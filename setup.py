f236df929903a0d8a9ed8ce846f804a8a06d8a63
Guzzii <guzzii316@gmail.com>
2018-10-15 16:25:20 -0500

    clean up setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, Extension, find_packages
 from sysconfig import get_paths
 
 
-# boost_python_lib = '/usr/local/boost_1_65_1/stage/lib/'
 python_include = get_paths()['include']
 
 compile_args = ['-g', '-std=c++1z', '-stdlib=libc++', ]
@@ -11,8 +10,7 @@ link_args = []  # ['-mmacosx-version-min=10.12']
 extension = Extension(name='machina.src.core',
                       sources=['machina/src/core.cpp'],
                       include_dirs=[python_include],
-                      # library_dirs=[boost_python_lib],
-                      libraries=['boost_python3'],
+                      libraries=['boost_python36'],
                       extra_compile_args=compile_args,
                       extra_link_args=link_args,
                       language='c++')
