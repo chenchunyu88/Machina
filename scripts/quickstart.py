de3e0ec365810ad7fa788aa65df77f3068883aee
Guzzii <guzzii316@gmail.com>
2018-10-16 11:47:38 -0500

    change default tax_fac

--- scripts/quickstart.py
+++ scripts/quickstart.py
@@ -8,6 +8,6 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == '__main__':
     plans = pd.read_csv(os.path.join(DIR, '../data/medical-plan-2018.csv'))
-    client = machina.Engine(plans, n_member=2, self_contri=1000, tax_fac=0.2)
-    client.fit(400)
+    client = machina.Engine(plans, n_member=2, self_contri=1000, tax_fac=0.15)
+    client.fit(iter=400)
     client.plot()
